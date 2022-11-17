25dee5931b90e9124c275af6c8ca3df51d1d8a36
wangwei <qingtian.ww@raycloud.com>
2022-11-18 00:41:53 +0800

    first commit

---
+++ leetcode_1351.go
@@ -0,0 +1,42 @@
+//leetcode_1351
+package main
+
+func countNegatives(grid [][]int) int {
+	m := len(grid[0])
+	n := len(grid)
+	if m <= 0 || n <= 0 {
+		return 0
+	}
+	num := m
+	x := m - 1
+	for i := m - 1; i >= 0; i-- {
+		if grid[0][i] >= 0 {
+			if i+1 <= m-1 {
+				x = i + 1
+				num = m - x
+			}
+			break
+		}
+	}
+	if n >= 2 {
+		for j := 1; j < n; j++ {
+			plus := true
+			for k := x; k >= 0; k-- {
+				if grid[j][k] >= 0 {
+					if k == m-1 {
+						plus = false
+					} else {
+						x = k + 1
+					}
+					break
+				} else if k == 0 {
+					x = 0
+				}
+			}
+			if plus {
+				num += m - x
+			}
+		}
+	}
+	return num
+}
---
+++ leetcode_1351_test.go
@@ -0,0 +1,12 @@
+package main
+
+import "testing"
+
+func TestCountNegatives(t *testing.T) {
+	grid := [][]int{{4, 3, 2, -1}, {3, 2, 1, -1}, {1, 1, -1, -2}, {-1, -1, -2, -3}}
+	num := countNegatives(grid)
+	t.Logf("num1: %d", num)
+	grid2 := [][]int{{3, 2}, {1, 0}}
+	num2 := countNegatives(grid2)
+	t.Logf("num2: %d", num2)
+}
