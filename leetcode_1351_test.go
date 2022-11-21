ad325531356e3a6c9d5cd65eaed505978170347f
PREPOET <ww.PREPOET@gmail.com>
2022-11-21 16:05:59 +0800

    modify test cases

--- leetcode_1351_test.go
+++ leetcode_1351_test.go
@@ -1,15 +1,22 @@
 package main
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+)
 
 func TestCountNegatives(t *testing.T) {
-	grid := [][]int{{4, 3, 2, -1}, {3, 2, 1, -1}, {1, 1, -1, -2}, {-1, -1, -2, -3}}
-	num := countNegatives(grid)
-	t.Logf("num1: %d", num)
-	grid2 := [][]int{{3, 2}, {1, 0}}
-	num2 := countNegatives(grid2)
-	t.Logf("num2: %d", num2)
-	grid3 := [][]int{{-1}}
-	num3 := countNegatives(grid3)
-	t.Logf("num2: %d", num3)
+	var tests = []struct {
+		input [][]int
+		want  int
+	}{
+		{[][]int{{4, 3, 2, -1}, {3, 2, 1, -1}, {1, 1, -1, -2}, {-1, -1, -2, -3}}, 8},
+		{[][]int{{3, 2}, {1, 0}}, 0},
+		{[][]int{{-1}}, 1},
+	}
+	for _, test := range tests {
+		if got := countNegatives(test.input); !reflect.DeepEqual(got, test.want) {
+			t.Errorf("countNegatives(%v) = %v", test.input, got)
+		}
+	}
 }
