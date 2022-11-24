c5d046ba6c1369a67f43edac3605356bf045462d
wangwei <qingtian.ww@raycloud.com>
2022-11-24 22:15:52 +0800

    No. Accepted

---
+++ leetcode_20.go
@@ -0,0 +1,37 @@
+package main
+
+import "container/list"
+
+// right -> left
+var brackets = map[uint8]uint8{
+	')': '(',
+	']': '[',
+	'}': '{',
+}
+
+func isValid(s string) bool {
+	l := len(s)
+	if l%2 == 1 {
+		// 成对出现 奇数直接返回
+		return false
+	}
+	stack := list.New()
+	for i := 0; i < l; i++ {
+		bracket := s[i]
+		left, ok := brackets[bracket]
+		if ok {
+			// right pop
+			tail := stack.Back()
+			if tail != nil && left == tail.Value {
+				// match
+				stack.Remove(tail)
+			} else {
+				return false
+			}
+		} else {
+			// left push
+			stack.PushBack(bracket)
+		}
+	}
+	return stack.Len() == 0
+}
---
+++ leetcode_20_test.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestIsValid(t *testing.T) {
+	var tests = []struct {
+		input string
+		want  bool
+	}{
+		{"{[]}", true},
+		{"()", true},
+		{"()[]{}", true},
+		{"(]", false},
+	}
+	for _, test := range tests {
+		if got := isValid(test.input); !reflect.DeepEqual(got, test.want) {
+			t.Errorf("isValid(%v) = %v", test.input, got)
+		}
+	}
+}
