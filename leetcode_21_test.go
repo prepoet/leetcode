6e2b499bec7368984ab7fd01e0c562f47783bfea
PREPOET <ww.PREPOET@gmail.com>
2022-11-25 14:20:37 +0800

    No.21 Accepted

--- leetcode_21_test.go
+++ leetcode_21_test.go
@@ -11,7 +11,9 @@ func TestMergeTwoLists(t *testing.T) {
 		input2 *ListNode
 		want   *ListNode
 	}{
-		{},
+		{
+			// 暂时没想到测试用例的快速构建 使用json反序列化是否可行
+		},
 	}
 	for _, test := range tests {
 		if got := mergeTwoLists(test.input1, test.input2); !reflect.DeepEqual(got, test.want) {
