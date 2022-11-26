12a5c62e825d69cc16878c67b4538902afadee85
wangwei <qingtian.ww@raycloud.com>
2022-11-26 17:20:35 +0800

    No.58 Accepted

---
+++ lc_58_length_of_last_word.go
@@ -0,0 +1,15 @@
+package main
+
+func lengthOfLastWord(s string) int {
+	l := len(s)
+	num := 0
+	for i := l - 1; i >= 0; i-- {
+		char := s[i]
+		if char != ' ' {
+			num++
+		} else if num > 0 {
+			break
+		}
+	}
+	return num
+}
---
+++ lc_58_length_of_last_word_test.go
@@ -0,0 +1,25 @@
+package main
+
+import "testing"
+
+func Test_lengthOfLastWord(t *testing.T) {
+	type args struct {
+		s string
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{"1", args{"Hello World"}, 5},
+		{"2", args{"   fly me   to   the moon  "}, 4},
+		{"3", args{"luffy is still joyboy"}, 6},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := lengthOfLastWord(tt.args.s); got != tt.want {
+				t.Errorf("lengthOfLastWord() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
