1b993078ea3c73c35a10205e73069b53561199e3
wangwei <qingtian.ww@raycloud.com>
2022-11-27 21:23:40 +0800

    No.66 Accepted

---
+++ lc_66_plus_one.go
@@ -0,0 +1,21 @@
+package main
+
+func plusOne(digits []int) []int {
+	l := len(digits)
+	for i := l - 1; i >= 0; i-- {
+		num := digits[i]
+		if num+1 == 10 {
+			digits[i] = 0
+			if i == 0 {
+				//生成新数组 这种情况是 每一位都是9
+				var array = make([]int, l+1)
+				array[0] = 1
+				return array
+			}
+		} else {
+			digits[i] = num + 1
+			return digits
+		}
+	}
+	return digits
+}
---
+++ lc_66_plus_one_test.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"reflect"
+	"testing"
+)
+
+func Test_plusOne(t *testing.T) {
+	type args struct {
+		digits []int
+	}
+	tests := []struct {
+		name string
+		args args
+		want []int
+	}{
+		{"1", args{[]int{1, 2, 3}}, []int{1, 2, 4}},
+		{"1", args{[]int{4, 3, 2, 1}}, []int{4, 3, 2, 2}},
+		{"1", args{[]int{9}}, []int{1, 0}},
+		{"1", args{[]int{7, 2, 8, 5, 0, 9, 1, 2, 9, 5, 3, 6, 6, 7, 3, 2, 8, 4, 3, 7, 9, 5, 7, 7, 4, 7, 4, 9, 4, 7, 0, 1, 1, 1, 7, 4, 0, 0, 6}}, []int{7, 2, 8, 5, 0, 9, 1, 2, 9, 5, 3, 6, 6, 7, 3, 2, 8, 4, 3, 7, 9, 5, 7, 7, 4, 7, 4, 9, 4, 7, 0, 1, 1, 1, 7, 4, 0, 0, 7}},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := plusOne(tt.args.digits); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("plusOne() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
