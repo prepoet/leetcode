2af09333c2c1f6cd50cde990e394c9cdb999ccd0
wangwei <ww9xlll@gmail.com>
2024-01-15 18:06:15 +0800

    lc_21

---
+++ lc_21_merge_two_sorted_lists.go
@@ -0,0 +1,38 @@
+package leetcode
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+func mergeTwoLists(list1 *ListNode, list2 *ListNode) *ListNode {
+	p1 := list1
+	p2 := list2
+	// 虚拟头节点
+	dummy := &ListNode{-1, nil}
+	p := dummy
+	for p1 != nil && p2 != nil {
+		if p1.Val <= p2.Val {
+			p.Next = p1
+			p1 = p1.Next
+		} else {
+			p.Next = p2
+			p2 = p2.Next
+		}
+		p = p.Next
+	}
+	if p1 != nil {
+		p.Next = p1
+	}
+	if p2 != nil {
+		p.Next = p2
+	}
+	return dummy.Next
+}
